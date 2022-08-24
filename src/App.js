84ca154ea1790aaa65444ea7a832cab931643043
Tiago Marin <tiago.lelinski@gmail.com>
2022-08-24 15:25:54 -0300

    setup react app and clean it up

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './assets/logo.webp';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img src={logo} alt="logo" ></img>
+      <h1>Bookstore</h1>
+    </div>
+  );
+}
+
+export default App;
